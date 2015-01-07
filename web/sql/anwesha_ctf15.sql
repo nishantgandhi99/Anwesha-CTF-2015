52bb56cfb1108883f38abdbd05dd1efa17ca089a
Amar Sharma <Amar Sharma@172.16.16.191>
2015-01-07 02:04:34 -0800

    Refractored complete code New sql file to be imported in mysql database

--- web/sql/anwesha_ctf15.sql
+++ web/sql/anwesha_ctf15.sql
@@ -1,22 +1,24 @@
 -- phpMyAdmin SQL Dump
--- version 3.2.0.1
+-- version 4.2.11
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: Nov 30, 2014 at 01:11 PM
--- Server version: 5.1.36
--- PHP Version: 5.3.0
+-- Host: 127.0.0.1
+-- Generation Time: Jan 07, 2015 at 08:37 AM
+-- Server version: 5.6.21
+-- PHP Version: 5.6.3
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-
-
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+CREATE USER 'ctf'@'localhost'; GRANT SELECT, INSERT, UPDATE, DELETE ON *.* TO 'ctf'@'localhost';
+CREATE DATABASE IF NOT EXISTS anwesha_ctf15;
+USE anwesha_ctf15;
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `anwesha_ctf14`
+-- Database: `anwesha_ctf15`
 --
 
 -- --------------------------------------------------------
@@ -29,8 +31,7 @@ CREATE TABLE IF NOT EXISTS `answers` (
   `Pid` int(11) NOT NULL,
   `Ppage` varchar(50) NOT NULL,
   `Panswer` varchar(50) NOT NULL,
-  `Ppoint` int(11) NOT NULL,
-  PRIMARY KEY (`Pid`)
+  `Ppoint` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
@@ -58,25 +59,8 @@ INSERT INTO `answers` (`Pid`, `Ppage`, `Panswer`, `Ppoint`) VALUES
 
 CREATE TABLE IF NOT EXISTS `auth` (
   `Id` varchar(50) NOT NULL,
-  `Pass` varchar(50) NOT NULL,
-  PRIMARY KEY (`Id`),
-  UNIQUE KEY `Id` (`Id`),
-  UNIQUE KEY `Id_2` (`Id`),
-  UNIQUE KEY `Id_5` (`Id`),
-  FULLTEXT KEY `Id_3` (`Id`),
-  FULLTEXT KEY `Id_4` (`Id`)
+  `Pass` varchar(50) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `auth`
---
-
-INSERT INTO `auth` (`Id`, `Pass`) VALUES
-('nishant', '584ae3a6ab89d67b476b2e732e7f73d6'),
-('xyz', 'd16fb36f0911f878998c136191af705e'),
-('a', '0cc175b9c0f1b6a831c399e269772661'),
-('R', '4b43b0aee35624cd95b910189b3dc231'),
-('ni', 'e6c151d449e1db05b1ffb5ad5ec656cf');
 
 -- --------------------------------------------------------
 
@@ -104,17 +88,6 @@ CREATE TABLE IF NOT EXISTS `problems` (
   `total` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `problems`
---
-
-INSERT INTO `problems` (`Id`, `p1`, `p2`, `p3`, `p4`, `p5`, `p6`, `p7`, `p8`, `p9`, `p10`, `p11`, `p12`, `p13`, `p14`, `p15`, `total`) VALUES
-('nishant', 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 15),
-('xyz', 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 20),
-('a', 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 55),
-('R', 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 20),
-('ni', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
-
 -- --------------------------------------------------------
 
 --
@@ -126,18 +99,31 @@ CREATE TABLE IF NOT EXISTS `userinfo` (
   `Id` varchar(10) NOT NULL,
   `clg` varchar(50) NOT NULL,
   `email` varchar(20) NOT NULL,
-  `ph_no` varchar(15) NOT NULL,
-  PRIMARY KEY (`Id`)
+  `ph_no` varchar(15) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `userinfo`
+-- Indexes for dumped tables
 --
 
-INSERT INTO `userinfo` (`Name`, `Id`, `clg`, `email`, `ph_no`) VALUES
-('nish', 'nishant', 'nishant', 'x@y.com', '123456'),
-('xyz', 'xyz', 'xyz', 'x@y.com', '123456'),
-('a', 'a', 'a', 'z@m.com', '1'),
-('r', 'R', 'r', 'x@y.com', '45'),
-('ni', 'ni', 'ni', 'v@c.com', '78');
+--
+-- Indexes for table `answers`
+--
+ALTER TABLE `answers`
+ ADD PRIMARY KEY (`Pid`);
 
+--
+-- Indexes for table `auth`
+--
+ALTER TABLE `auth`
+ ADD PRIMARY KEY (`Id`), ADD UNIQUE KEY `Id` (`Id`), ADD UNIQUE KEY `Id_2` (`Id`), ADD UNIQUE KEY `Id_5` (`Id`), ADD FULLTEXT KEY `Id_3` (`Id`), ADD FULLTEXT KEY `Id_4` (`Id`);
+
+--
+-- Indexes for table `userinfo`
+--
+ALTER TABLE `userinfo`
+ ADD PRIMARY KEY (`Id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
